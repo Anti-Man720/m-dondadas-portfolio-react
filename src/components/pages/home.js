ce36b402c8f79eef82630152abb5d7d6b6accc77
Anti-Man720 <mikealphazuluc@gamil.com>
2023-04-20 03:09:01 -0500

    blog_modal_icons

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -3,13 +3,9 @@ import React from "react";
 import PortfolioContainer from "../portfolio/portfolio-container";
 
 export default function () {
-    return (
-        <div>
-            <PortfolioContainer />
-        </div>
-    );
+  return (
+    <div>
+      <PortfolioContainer />
+    </div>
+  );
 }
-
-            
-            
-            
