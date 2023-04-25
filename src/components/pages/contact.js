062837c8f7a86ba6a4da5141f922bc3306edd182
Anti-Man720 <mikealphazuluc@gamil.com>
2023-04-24 22:00:42 -0500

    about_the_contact

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import contactPagePicture from "../../../static/assets/images/auth/login.jpg";
 
 export default function () {
-    return (
-        <div>
-            <h1>Contact</h1>
-            <p>This is the contact page</p>
-            <h2>THIS IS HOW YOU contact me on MY THOUGHTS AND PLANS FOR ABSOLUTE WORLD DOMINATION</h2>
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + contactPagePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <div className="right-column">
+        <div className="contact-bullet-points">
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="phone" />
+            </div>
+
+            <div className="text">555-666-5555</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="envelope" />
+            </div>
+
+            <div className="text">theConquerer@absolutedominion.gov</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="map-marked-alt" />
+            </div>
+
+            <div className="text">Andromeda, Milky Way Galaxy</div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
